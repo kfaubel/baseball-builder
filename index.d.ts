22d91a8637029de0bce7f75539114b2eb4bd66d1
Ken Faubel <ken@faubel.org>
2021-08-03 18:46:02 -0400

    Try adding more to index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,15 @@
 declare module "baseball-builder";
+
+export interface ImageResult {
+    expires: string;
+    imageType: string;
+    imageData: jpeg.BufferRet | null;
+}
+export declare class BaseballImage {
+    private baseballData;
+    private dayList;
+    private logger;
+    private cache;
+    constructor(logger: Logger, cache: Cache);
+    getImage(teamAbbrev: string): Promise<ImageResult | null>;
+}
