89812eba9959443968cb9ec11dffb83cee7ef351
kfaubel <ken@faubel.org>
2021-08-08 08:17:16 -0400

    Update image.js and image.d.ts for BaseballBuilder

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,27 @@
 declare module "baseball-builder";
 
-export interface ImageResult {
-    expires: string;
-    imageType: string;
-    imageData: jpeg.BufferRet | null;
+export interface LoggerInterface {
+    error(text: string): void;
+    warn(text: string): void;
+    log(text: string): void;
+    info(text: string): void;
+    verbose(text: string): void;
+    trace(text: string): void;
 }
-export declare class BaseballImage {
-    constructor(logger: LoggerInterface, cache: KacheInterface);
-    getImage(teamAbbrev: string): Promise<ImageResult | null>;
+
+export interface KacheInterface {
+    get(key: string): unknown;
+    set(key: string, newItem: unknown, expirationTime: number): void;
+}
+
+export interface ImageWriterInterface {
+    saveImage(fileName: string, buf: Buffer): void;
+}
+
+export declare class BaseballBuilder {
+    private logger;
+    private cache;
+    private writer;
+    constructor(logger: LoggerInterface, cache: KacheInterface, writer: ImageWriterInterface);
+    CreateImages(params: any): Promise<boolean>;
 }
--- index.js
+++ index.js
@@ -2,5 +2,5 @@
 // This is used when embedding this in a larger solution
 // Use app.ts for local testing and to build a stand alone solution
 
-const BaseballImage = require("./build/BaseballImage");
-module.exports = BaseballImage;
+const BaseballBuilder = require("./build/BaseballBuilder");
+module.exports = BaseballBuilder;
